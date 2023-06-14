83787793c8d3ecd5cf2ffc0d6e87c202343d32a8
rruiqiu <qiur12.mcmaster.ca>
2023-06-14 09:39:10 -0400

    use-refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   //FC stand for FunctionComponent
   const todos = [{ id: 't1', text: 'Finish the course' }];
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
